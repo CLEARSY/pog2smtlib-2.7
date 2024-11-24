e494253ad9f10532bae5861cb34ad7b43779a0c9
dde-cls <52076107+dde-cls@users.noreply.github.com>
2024-11-24 15:49:29 +0100

    Source code formatting follows .clang-format setup [#10]

--- src/POGLoader/pog.h
+++ src/POGLoader/pog.h
@@ -18,108 +18,111 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef POG_H
 #define POG_H
 
-#include <vector>
 #include <variant>
+#include <vector>
 using std::variant;
 
-#include <string>
 #include <QDomDocument>
+#include <string>
 
-#include "pred.h"
 #include "gpred.h"
+#include "pred.h"
 
 namespace pog {
 
-    class PogException : public std::exception
-    {
-        public:
-            PogException(const std::string desc):description{desc}{};
-            ~PogException() throw(){};
-            const char *what() const throw(){ return description.c_str(); };
-        private:
-            std::string description;
-    };
+class PogException : public std::exception {
+public:
+    PogException(const std::string desc) : description{desc} {};
+    ~PogException() throw() {};
+    const char *what() const throw() { return description.c_str(); };
 
-    class PO {
-        public:
-            std::string tag;
-            std::vector<int> localHypsRef;
-            Pred goal;
-            PO( const std::string &tag,
-                    const std::vector<int> &localHypsRef,
-                    Pred &&goal)
-                : tag{tag},
-                localHypsRef{localHypsRef},
-                goal{std::move(goal)}
-            {}
-            PO copy() const {
-                return PO(tag,localHypsRef,goal.copy());
-            }
-    };
+private:
+    std::string description;
+};
 
-    class POGroup {
-        public:
-            std::string tag;
-            size_t goalHash;
-            std::vector<std::string> definitions;
-            std::vector<Pred> hyps; // chaque element d'une conjonction est stocké séparement
-            std::vector<Pred> localHyps; // chaque element d'une conjonction est stocké séparement
-            std::vector<PO> simpleGoals;
-            POGroup(const std::string &tag,
-                    size_t goalHash,
-                    const std::vector<std::string> &definitions,
-                    std::vector<Pred> &&hyps,
-                    std::vector<Pred> &&localHyps,
-                    std::vector<PO> &&simpleGoals)
-                    : tag{tag}
-                    , goalHash{goalHash}
-                    , definitions{definitions}
-                    , hyps{std::move(hyps)}
-                    , localHyps{std::move(localHyps)}
-                    , simpleGoals{std::move(simpleGoals)}
-            {}
-    };
+class PO {
+public:
+    std::string tag;
+    std::vector<int> localHypsRef;
+    Pred goal;
+    PO(const std::string &tag, const std::vector<int> &localHypsRef,
+       Pred &&goal)
+        : tag{tag}, localHypsRef{localHypsRef}, goal{std::move(goal)} {}
+    PO copy() const { return PO(tag, localHypsRef, goal.copy()); }
+};
 
-    class Set {
-        public:
-            Set(const TypedVar &setName,const std::vector<TypedVar> &elts):
-                setName{setName},elts{elts}{};
-            TypedVar setName;
-            std::vector<TypedVar> elts;
+class POGroup {
+public:
+    std::string tag;
+    size_t goalHash;
+    std::vector<std::string> definitions;
+    std::vector<Pred>
+        hyps; // chaque element d'une conjonction est stocké séparement
+    std::vector<Pred>
+        localHyps; // chaque element d'une conjonction est stocké séparement
+    std::vector<PO> simpleGoals;
+    POGroup(const std::string &tag, size_t goalHash,
+            const std::vector<std::string> &definitions,
+            std::vector<Pred> &&hyps, std::vector<Pred> &&localHyps,
+            std::vector<PO> &&simpleGoals)
+        : tag{tag}, goalHash{goalHash}, definitions{definitions},
+          hyps{std::move(hyps)}, localHyps{std::move(localHyps)},
+          simpleGoals{std::move(simpleGoals)} {}
+};
 
-            static int compare(const Set &v1, const Set& v2);
-            inline bool operator==(const Set& other) const { return compare(*this,other) == 0; }
-            inline bool operator!=(const Set& other) const { return compare(*this,other) != 0; }
-            inline bool operator< (const Set& other) const { return compare(*this,other) <  0; }
-            inline bool operator> (const Set& other) const { return compare(*this,other) >  0; }
-            inline bool operator<=(const Set& other) const { return compare(*this,other) <= 0; }
-            inline bool operator>=(const Set& other) const { return compare(*this,other) >= 0; }
-    };
+class Set {
+public:
+    Set(const TypedVar &setName, const std::vector<TypedVar> &elts)
+        : setName{setName}, elts{elts} {};
+    TypedVar setName;
+    std::vector<TypedVar> elts;
 
-    class Define {
-        public:
-            Define(const std::string &name, size_t hash=0):name{name},hash{hash}{};
-            std::string name;
-            size_t hash;
-            std::vector<variant<Set, Pred>> contents;
-    };
+    static int compare(const Set &v1, const Set &v2);
+    inline bool operator==(const Set &other) const {
+        return compare(*this, other) == 0;
+    }
+    inline bool operator!=(const Set &other) const {
+        return compare(*this, other) != 0;
+    }
+    inline bool operator<(const Set &other) const {
+        return compare(*this, other) < 0;
+    }
+    inline bool operator>(const Set &other) const {
+        return compare(*this, other) > 0;
+    }
+    inline bool operator<=(const Set &other) const {
+        return compare(*this, other) <= 0;
+    }
+    inline bool operator>=(const Set &other) const {
+        return compare(*this, other) >= 0;
+    }
+};
 
-    class Pog {
-        public:
-            std::vector<Define> defines;
-            std::vector<POGroup> pos;
-    };
+class Define {
+public:
+    Define(const std::string &name, size_t hash = 0)
+        : name{name}, hash{hash} {};
+    std::string name;
+    size_t hash;
+    std::vector<variant<Set, Pred>> contents;
+};
 
-  struct Options {
-        QStringList ressources;
-        bool generateWDPOs = false;
-        bool overflow = false;
-        bool obvious = false;
-        bool verbose = true;
-        int limit = 10000;
-    };
+class Pog {
+public:
+    std::vector<Define> defines;
+    std::vector<POGroup> pos;
+};
 
-    Pog read(const QDomDocument &pog);
-}
+struct Options {
+    QStringList ressources;
+    bool generateWDPOs = false;
+    bool overflow = false;
+    bool obvious = false;
+    bool verbose = true;
+    int limit = 10000;
+};
+
+Pog read(const QDomDocument &pog);
+} // namespace pog
 
 #endif // POG_H
