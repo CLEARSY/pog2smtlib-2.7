e7f4fd8bd605fe805b6fc921a0bbf2cb4f3da39e
David Deharbe <david.deharbe@clearsy.com>
2025-08-18 08:59:28 +0000

    support for injections, surjections, bijections (5.15)

---
+++ src/pog2smtlib-2.7/constructs/expression/totalsurjections.cpp
@@ -0,0 +1,48 @@
+#include <fmt/core.h>
+
+#include "../../bconstruct.h"
+#include "../../btype-symbols.h"
+#include "../../parameters.h"
+#include "../../translate-token.h"
+#include "btype.h"
+
+namespace BConstruct::Expression {
+
+static constexpr std::string_view SCRIPT = R"(
+(declare-fun {0} ({1} {2}) {3})
+(assert (!
+  (forall ((e1 {1}) (e2 {2}))
+    (forall ((f {4}))
+      (= ({5} f ({0} e1 e2))
+         (and ({5} f ({9} e1 e2))
+                ({5} f (|surjections {6} {7}| e1 e2))))))
+  :named |ax:def.tsurj {8}|))
+)";
+
+Total_Surjection::Total_Surjection(const BType &U, const BType &V)
+    : BinaryBType(U, V) {
+  const auto PU = BType::POW(U);
+  const auto PV = BType::POW(V);
+  const auto UxV = BType::PROD(U, V);
+  const auto PUxV = BType::POW(UxV);
+  const auto PPUxV = BType::POW(PUxV);
+  m_script =
+      fmt::format(SCRIPT,
+                  /*0*/ smtSymbol(Expr::BinaryOp::Total_Surjections, U, V),
+                  /*1*/ symbol(PU),
+                  /*2*/ symbol(PV),
+                  /*3*/ symbol(PPUxV),
+                  /*4*/ symbol(PUxV),
+                  /*5*/ smtSymbol(Pred::ComparisonOp::Membership, PUxV),
+                  /*6*/ symbolInner(U),
+                  /*7*/ symbolInner(V),
+                  /*8*/ symbolInner(UxV),
+                  /*9*/ smtSymbol(Expr::BinaryOp::Total_Functions, U, V));
+  m_label = "-->>";
+  m_prerequisites.insert(
+      {std::make_shared<BConstruct::Expression::Total_Function>(U, V),
+       std::make_shared<BConstruct::Expression::Surjection>(U, V)});
+  m_debug_string = fmt::format("-->>_<{},{}>", U.to_string(), V.to_string());
+}
+
+};  // namespace BConstruct::Expression
